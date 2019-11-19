586490f6c9e6cee33c9c982bf39f28a7f40b696a
JustinLee28 <vision5343@gmail.com>
2019-11-19 17:30:53 +0000

    My first commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<html>
+  <head>
+    <title>PHP Test</title>
+  </head>
+  <body>
+    <?php echo '<p>Hello World! Here are all of my Links [Justin Lee Period 2]</p>'; ?> 
+    <ul>
+      <li><a href="index.html">index.html</a></li></br>
+      <!-- Please make sure to change all the #'s to the proper link names-->
+      <li><a href="Lee_AboutMe.html">Lee_AboutMe.html</a></li></br>
+      <li><a href="Lee_Projects.html">Lee_Projects.html</a></li></br>
+      <li><a href="Lee_Resume.html">Lee_Resume.html</a></li></br>
+      <li><a href="Lee_ContactMe.html">Lee_ContactMe.html</a></li></br>
+    <ul>
+  </body>
+</html>
